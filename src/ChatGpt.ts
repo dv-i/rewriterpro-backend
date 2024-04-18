97caccc170b31f91f5df2db95804e10f0f192289
KamranKhan11 <thekamransj@gmail.com>
2024-04-18 15:33:25 +0500

    update to gpt4

--- src/ChatGpt.ts
+++ src/ChatGpt.ts
@@ -23,7 +23,7 @@ export const getResponseToAPrompt = async ({
   };
 
   const requestData = {
-    model: "gpt-3.5-turbo-0125",
+    model: "gpt-4-0125-preview",
     messages: [
       {
         role: "user",
